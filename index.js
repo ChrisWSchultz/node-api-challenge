ce9f1f128fe02b3c8879f9d33312e22f796ff809
Christopher W Schultz <chris.william.schultz@gmail.com>
2020-08-14 18:57:13 -0600

    added start script

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const projectsRouter =  require('./routers/projects')
 const actionsRouter = require('./routers/actions')
 
 const app = express()
-const port = 8080
+const port = process.env.PORT || 8080
 
 app.use(express.json())
 app.use(cors())
